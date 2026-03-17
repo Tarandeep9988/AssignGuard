c8d99c9ae9cc6fc9b5e7a375af12cf3313c42ee6
Tarandeep Singh <tarandeep65singh@gmail.com>
2026-03-17 14:41:30 +0530

    updated custom error file

--- backend/utils/AppError.ts
+++ backend/utils/AppError.ts
@@ -1,6 +1,12 @@
-class AppError extends Error {
+export class AppError extends Error {
   statusCode: number;
-  constructor(message: string, statusCode: number) {
+  constructor({
+    message,
+    statusCode,
+  }: {
+    message: string;
+    statusCode: number;
+  }) {
     super(message);
     this.statusCode = statusCode;
   }
