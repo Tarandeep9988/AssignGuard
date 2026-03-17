ecfd274c2f712c90251be0ff6e7a9123d768115e
Tarandeep Singh <tarandeep65singh@gmail.com>
2026-03-17 14:21:52 +0530

    Created assignment model

---
+++ backend/models/Assignment.ts
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const assignmentSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  dueDate: {
+    type: Date,
+    required: true,
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+}, { timestamps: true });
+
+export default mongoose.model('Assignment', assignmentSchema);
