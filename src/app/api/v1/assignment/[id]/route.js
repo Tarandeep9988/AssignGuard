f9e135bc803cbe4ce1f47b41788be70cbb1882f9
Tarandeep Singh <tarandeep65singh@gmail.com>
2026-03-20 21:20:11 +0530

    project restructure

--- src/app/api/v1/assignment/[id]/route.js
+++
@@ -1,8 +0,0 @@
-import { deleteAssignmentController } from "@/controllers/assignment";
-import connectDb from "@/lib/mongodb";
-
-export async function DELETE(request, { params }) {
-  await connectDb();
-  const { id } = await params;
-  return deleteAssignmentController({ id });
-}
--- src/app/api/v1/assignment/[id]/submission/route.js
+++
@@ -1,8 +0,0 @@
-import connectDb from "@/lib/mongodb";
-import { getSubmissionsController } from "@/controllers/submission";
-
-export async function GET(request, {params}) {
-  await connectDb();
-  const { id } = await params;
-  return getSubmissionsController({ assignmentId: id });
-}
