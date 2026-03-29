2b749b971f3ba6d2fa2bc4810c7c43986d3e702b
Tarandeep Singh <tarandeep65singh@gmail.com>
2026-03-29 13:11:52 +0530

    changed in Submssion model

--- backend/models/Submission.ts
+++ backend/models/Submission.ts
@@ -1,16 +1,8 @@
 import mongoose from 'mongoose';
 
 const submissionSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  dueDate: {
-    type: Date,
+  content: {
+    type: String, 
     required: true,
   },
   userId: {
