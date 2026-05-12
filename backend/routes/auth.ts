4da20a16c5f591da60a09332828a8818fa814e81
Tarandeep Singh <connect.tarandeep@gmail.com>
2026-05-12 17:59:31 +0530

    fixed auth routes -> formatting

--- backend/routes/auth.ts
+++ backend/routes/auth.ts
@@ -1,10 +1,14 @@
 import express, { type Router } from 'express';
 import authController from "../controllers/auth.js";
+import { authenticate } from '../middlewares/auth.js';
+
 const authRouter: Router = express.Router();
 
 authRouter.post("/login", authController.loginHandler);
 authRouter.post("/register", authController.registerHandler);
-authRouter.post("/logout", authController.logoutHandler);
-authRouter.get("/verify", authController.verifyHandler);
+
+// Protected routes
+authRouter.post("/logout", authenticate, authController.logoutHandler);
+authRouter.get("/verify", authenticate, authController.verifyHandler);
 
 export default authRouter;
